5f3ad404a5003b250d498ee064023f284adc6d7f
AmtazAhmedGazi <amtazahmedgazi@gmail.com>
2026-01-07 06:44:48 +0600

    fixed environment variable

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <MyContainer className="navbar ">
+    <MyContainer className="navbar shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMnBcLZT0guz4_qXnqt6USroEwTDx-vG8",
-  authDomain: "toy-land-3bfb2.firebaseapp.com",
-  projectId: "toy-land-3bfb2",
-  storageBucket: "toy-land-3bfb2.firebasestorage.app",
-  messagingSenderId: "631813004550",
-  appId: "1:631813004550:web:da49d27533a54d90b31287"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
